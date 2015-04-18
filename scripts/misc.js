4a1b7bff4c6429147aeeebb4b2b7e654d74c5e7f
Khoi Nguyen <khoinguyen998@gmail.com>
2015-04-17 20:18:33 -0700

    upd

--- scripts/misc.js
+++ scripts/misc.js
@@ -7,6 +7,15 @@ module.exports = function(input, out) {
 		out("Source available on GitHub: " + "http://github.com/potasmic/schbot");
 	}
 	else if ( what == "help" ) {
-		out("Available commands: cum, gal, sick, define, wiki.");
+		out("Available commands: define, wiki, source, help, sick, gal, cum. PM !help for more.");
+		if ( extra.from.substr(0,1) != "#" ) {
+			out("cum [keywords] [, keyword2] | [filter] [, filter 2] | [rating above]: Returns a random image from Gelbooru relating to Shota", true);
+			out("gal: returns a random image from the Shotachan Gallery", true);
+			out("define [text] : Fetch definition of a word from UrbanDictionary. ", true);
+			out("wiki [text] : Fetch summary of Wikipedia article if found. " , true);
+			out("source: Returns link to GitHub Repository of this bot.", true);
+			out("help: Shows this help message.", true);
+			out("sick: Returns a one of the top daily jokes from Sickipedia. ", true);
+		}
 	}
 }
