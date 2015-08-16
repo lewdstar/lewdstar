37defd8c872b60e34794e6e0ee92470ffe48df33
Khoi Nguyen <khoinguyen998@gmail.com>
2015-08-15 22:37:44 -0700

    voice

--- scripts/voiced-.js
+++ scripts/voiced-.js
@@ -1,6 +1,6 @@
 
 module.exports = function(ins, channels, bot) {
-	if( ins[2] !== "v" ) return;
+	if( ins[2] !== "v" & ins[3] !== bot.nick) return;
 
 	bot.say(ins[0], ins[1] + ": Fuck you.");
 	return;
