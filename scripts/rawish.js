f5a1e144ebe0f4b30f7a6d052cdafd0bb06ddbab
Khoi Nguyen <khoinguyen998@gmail.com>
2015-09-01 20:45:48 -0700

    EXPERIMENTAL: pipe

--- scripts/rawish.js
+++ scripts/rawish.js
@@ -19,4 +19,25 @@ module.exports = function(input, chans, bot) {
 			bot.say(raw.args[1].split(" ")[2], "Hello. I have been summoned by " + raw.user +". "+bot.nick+ " is ready to rock & roll!");
 		});
 	}
+	//le say
+	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,5) == "$ say" && admins.indexOf(raw.user) !== -1 ) {
+		bot.say(raw.args[1].split(" ")[2], raw.args[1].split(" ").slice(3).join(" "));
+	}
+	//le pipe (experimental and dangerous) 
+	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,6) == "$ pipe" && admins.indexOf(raw.user) !== -1 ) {
+		var source = raw.args[1].split(" ")[2];
+		var target = raw.args[1].split(" ")[3];
+		
+		if( source.split("")[0] == "#" ) {
+			bot.addListener("message"+source, function(from, message) {
+				bot.say(target, from + ": " + message);
+			});
+		} else { // PMs to target
+			bot.addListener("pm", function(from, message) {
+				if( from == source ) {
+					bot.say(target, message);
+				}
+			});
+		}
+	}
 }
