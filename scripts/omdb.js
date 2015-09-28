70254a982e4ff375cd7450694e102ce7189899c4
Khoi Nguyen <khoinguyen998@gmail.com>
2015-09-27 18:31:16 -0700

    faith

--- scripts/omdb.js
+++ scripts/omdb.js
@@ -6,17 +6,17 @@ module.exports = function( input, out) {
 	var season = "", episode = "";
 
 	if ( /season\s[0-9]+/i.test(input.regex[0]) == true ) {
-		season = "&Season=" + (/season\s([0-9]+)/i).exec(input.regex[0])[1];
+		season = "&Season=" + encodeURIComponent((/season\s([0-9]+)/i).exec(input.regex[0])[1]);
 	}
 	if ( season && /episode\s[0-9]+/i.test(input.regex[0]) == true ) {
-		episode = "&Episode=" + (/episode\s([0-9]+)/i).exec(input.regex[0])[1];
+		episode = "&Episode=" + encodeURIComponent((/episode\s([0-9]+)/i).exec(input.regex[0])[1]);
 	}
 
 	var title = input.regex[1].replace(/season\s[0-9]+/i,"").replace(/episode\s[0-9]+/i,"").trim();
 
 	var res = "";
 
-	http.get("http://www.omdbapi.com/?t=" + encodeURIComponent(title + season + episode), function(rep) {
+	http.get("http://www.omdbapi.com/?t=" + encodeURIComponent(title)+ season + episode, function(rep) {
 
 		rep.on("data", function(dat) {
 			res += dat;
