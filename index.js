9b3e62c65f01176f6b753ad025fdebbe3876d8b5
khoin <khoinguyen998@gmail.com>
2016-08-21 16:44:29 -0700

    migrate to Discord

--- index.js
+++ index.js
@@ -1,261 +1,47 @@
 process.stdin.setEncoding('utf8');
-//node index.js -n nick -s server -p port -e exec.json [-h] [-i]
-/**
-	Definition for exec.txt:
 
-	Content:
-	{	messageListeners: [
-			{
-				script: "./scripts/cappy.js",
-				channels: ["#test", "#debug"],
-				regex: "^!test:(.*)",
-				private: false
-			},
-			{
-				script: "./scripts/anotherScript",
-				channels: [],
-				regex: "^PMOnly",
-				private: true
-			}
-		],
-		customListeners: [{
-			event: "action",
-			script: "./scripts/wow.js",
-			autostart: true
-		}]
-	
-	}
-**/
+const Discord   = require('discord.js');
+const fs  		= require('fs');
 
-/**
-	Definition for processing scripts:
+var bot = new Discord.Client();
 
-	//Simple CAPLOCKSPINATOR!
-
-	module.exports = function(input, callback) {
-		callback( input[from]+ " said: ");
-		callback( input[1].toUpperCase() );
-		callback( "Hi privately!", true);
-	}
-
-**/
-var irc = require('irc');
-var os  = require('os');
-var fs  = require('fs');
-var color = require('ansi-color').set;
-
-var port = process.argv.indexOf('-p') > 0 ? process.argv[process.argv.indexOf('-p')+1] : "6697";
-var exec = process.argv.indexOf('-e') > 0 ? process.argv[process.argv.indexOf('-e')+1] : null;
-var nick = process.argv.indexOf('-n') > 0 ? process.argv[process.argv.indexOf('-n')+1] : "fuccboi";
-var serv = process.argv.indexOf('-s') > 0 ? process.argv[process.argv.indexOf('-s')+1] : null;
-
-if (exec == null) {
-	console.log(color("No executing file was given. Exiting...", "red"));
-	return;
-} 
-if (serv == null) {
-	console.log(color("No server was given. Exiting...", "red"));
-}
-
-
-if ( process.argv.indexOf('-h') !== -1 ) {
-	var txt = '\n';
-	txt += color('      PotIRCl Bot.      ','black+green_bg');
-	txt += '\n      ------------ \n';
-	txt += 'Executing Syntax: node index.js -n nick -s irc.server.net -p 6667 -e exec.txt \n';
-	txt += '\nOptions: \n';
-	txt += '    -n      Nick for the bot. \n';
-	txt += '    -s      IRC network server address. \n';
-	txt += '    -p      Port\n';
-	txt += '    -e      Executing file (See doc for more) \n';
-	txt += '    -i      Just a bonus paramater.\n';
-	console.log(txt);
-	return;
-}
-
-var bot = new irc.Client(serv, nick, {
-	sasl: true,
-	userName: "fuccboi",
-	realName: "fuccboi",
-	autoConnect: true,
-	password: "shotachan.Mdyz2ydzEUxUh1Tw3bgO",
-	port: 6697,
-	secure: true,
-	certExpired: true,
-	selfSigned: true,
-	showErrors: true
+bot.loginWithToken('MjE2ODA0MjE3ODYyNDg4MDY1.CpuzVw.9FtcJtASmr9YIqRQIHBvu9X_IM8', (err) => {
+	console.log('Login Error:', err);
 });
 
-var globalLastMessage = [];
-
 bot.features = {}; // Reserved spaces for modules.
-bot.color = irc.colors.wrap;
 
 try {
-	var configuration = JSON.parse(fs.readFileSync(exec).toString());
+	var configuration = JSON.parse(fs.readFileSync(process.argv[1+process.argv.indexOf("-e")]).toString());
 	bot.features.config = configuration;
 	if( (configuration.messageListeners == undefined) && (configuration.customListeners == undefined) ) { throw Error("No listeners given."); }
-	if( !(configuration.config.negations instanceof Array) ) { throw Error("Negation list is not an array. You can leave it blank, FYI."); }
-	if( !(configuration.config.quitMessage instanceof Array) ) { throw Error("No Quit message? You can leave it black, FYI."); }
-
 } catch(e) {
-	console.log(color("Corrupted or invalid JSON file.", "red"));
+	console.log("Corrupted or invalid JSON file.");
 	console.log(e);
 	process.exit();
 }
 
-bot.addListener('error', function(e) {
-	console.log(color("Error: ", "red"));
-	console.log(e);
-});
+bot.on('message', (message) => {
+	if(message.author.username == bot.user.username) return;
 
-//Connected
-var _LISTENED = 0;
-bot.addListener('registered', function(msg) {
-	/** NS
-	console.log("-   " + color("Authing: ", "cyan_bg") + "NickServ");
+	bot.features.config.messageListeners.forEach( listener => {
+		var regex = new RegExp(listener.regex);
+		if(  regex.test(message.content) ) { 
 
-	if ( configuration.config.register.bool == true ) {
-		if ( bot.nick !== nick ) {
-			bot.say("NickServ","GHOST "+ configuration.config.register.username +" "+ configuration.config.register.password);
-		}
-		bot.send ("NICK", nick);
-		bot.say("NickServ","IDENTIFY " + configuration.config.register.password );
-	}
-	**/
-	if( _LISTENED === 0 ) initMessageListeners(); 
-	_LISTENED++;
-});
+			var processor = require(listener.script);
 
-var joinedChannels = [];
-
-function initMessageListeners(callback) {
-	bot.setMaxListeners(100);
-	//Join channels
-	configuration.messageListeners.forEach( function(listener) {
-		if( listener.channels.length > 0 ) {
-			listener.channels.forEach( function(channel) {
-				if( joinedChannels.indexOf(channel) == -1 && configuration.config.negations.indexOf(channel) == -1 ) {
-					console.log("--  " + color("Joining: ", "green_bg") + channel);
-					bot.join(channel);
-					joinedChannels.push(channel);
+			processor({
+				from: message.channel,
+				regex: regex.exec(message.content)
+			}, (txt, pm) => {
+				if( !pm ) { bot.sendMessage(message.channel, txt) } else {
+					bot.sendMessage(message.authro, txt);
 				}
-			})
-		}
-	});
-
-	//Attach Message Listeners
-	configuration.messageListeners.forEach( function(listener) {
-		console.log("--- " + color("Binding: ", "yellow_bg") + listener.script + " with " + color(listener.regex,"green") + " in:");
-
-		var processor = require(listener.script);
-
-		//Private?
-		if(listener.private == true) {
-			bot.addListener("pm", function(from, message) {
-
-				var input = 
-					{
-						from: from,
-						regex: (new RegExp(listener.regex)).exec(message.trim())
-					};
-				var extra = 
-					{
-						from: from,
-						bot: bot
-					};
-
-				if( (new RegExp(listener.regex)).test( message.trim() ) ) {
-					try {
-						processor(input, function(txt) {
-							bot.say(from, txt);
-						}, extra);
-					} catch(e) {
-						bot.say(from, "index.js[172] Error: "+e.message);
-						console.log(e);
-					}
-				}
-
+			}, {
+				from: message.channel.toString().substr(1, message.channel.toString().length-2),
+				bot: bot
 			});
+
 		}
-		
-		//Channel.
-		listener.channels.forEach(function(channel) {
-			//create Last Message holder
-			globalLastMessage[channel] = "";
-
-			//Negation channels
-			if( configuration.config.negations.indexOf(channel) !== -1 ) return;
-
-			//Logging
-			console.log("    +--> " + color(channel,"yellow"));
-
-			bot.addListener("message"+channel, function( from, message ) {
-
-				var input = 
-					{ 
-						from: from,
-					 	regex: (new RegExp(listener.regex)).exec(message.trim() )
-					};
-				var extra = 
-					{
-						from: channel,
-						bot: bot
-					};
-					
-				if( (new RegExp(listener.regex)).test( message.trim() ) ) {
-						processor(input, function(txt, pm) {
-							if(!pm) {
-								bot.say(channel, txt);
-							} else {
-								bot.say(from, txt);
-							}
-						}, extra);
-				} else if(  (new RegExp(listener.regex)).test( message.trim() +" "+ globalLastMessage[channel])  ) {
-					console.log(message.trim() + " " + globalLastMessage[channel])
-						processor({
-								from: from,
-								regex: (new RegExp(listener.regex)).exec(message.trim() + " " + globalLastMessage[channel] ) 
-							}, function(txt, pm) {
-							if(!pm) {
-								bot.say(channel, txt);
-							} else {
-								bot.say(from, txt);
-							}
-						}, extra);
-				} else {
-					globalLastMessage[channel] = (message.trim().substr(0,1) == "!" )? globalLastMessage[channel] : message.trim();
-				}		
-				
-			});
-		});
-
-	});
-
-	//Attach RAW Custom Listeners
-	configuration.customListeners.forEach( function(listener) {
-		console.log("----"+color("Binding: ", "magenta_bg") + listener.script + " with event " + color(listener.event, "green"));
-
-		var processor = require(listener.script);
-
-		bot.addListener(listener.event, function() {
-			processor(arguments, listener.channels, bot);
-		});
-	});
-
-	if (callback instanceof Function) callback();
-} 
-
-//Handling exits
-function properKill() {
-	var bye = configuration.config.quitMessage[Math.floor(configuration.config.quitMessage.length*Math.random())];
-
-	bot.disconnect(bye, function() {
-		
-	});
-	setTimeout(function() { process.exit("Bye"); }, 1000 );
-}
-
-
-process.on('SIGINT', properKill);
-process.on('SIGTERM', properKill);
+	})
+});
--- scripts/rawish.js
+++ scripts/rawish.js
@@ -1,120 +1,21 @@
-var md5 = require('md5');
 
-module.exports = function(input, chans, bot) {
-	var raw = input[0];
-		if ( !(bot.features.config.admins instanceof Array) ) { 
-			bot.features.config.admins = ["shotacat","aspect", "therobotsghost", "contxt", "kenpari", "jamaic", "tweaker","shadoukun"]; 
-		}
-	var admins = bot.features.config.admins;
-	var authkeys = ["1646a5b49c8eb03883db1386c0084a74"];
-	var forbidden = ["pipe", "config"];
-
-	//heyitsme
-	if (raw.command =="PRIVMSG" && raw.args[1].substr(0,10) == "$ heyitsme" ) { 
-		if( authkeys.indexOf(md5(raw.args[1].trim().split(" ")[2])) !== -1 ) { 
-			if( raw.args[0].substr(0,1) == "#" ) {
-				bot.say(raw.args[0], "Are you stupid?");
-			} else {
-				admins.push(raw.user);
-				bot.say(raw.nick, "You are now authenticated.");
-			}
-		} else {
-			if( raw.args[0].substr(0,1) == "#" ) {
-				bot.say(raw.args[0], "Are you stupid??");
-			} else {
-				bot.say(raw.nick, "No.");
-			}
-		}
-	}
+module.exports = function(input, out, extra) {
+	var bot = extra.bot;
 
 	//le flush stuffs
-	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,7) == "$ flush" ) {
-		var what = raw.args[1].split(" ")[2];
+	if( input.regex.input.substr(0,7) == "$ flush" ) {
+		var what = input.regex.input.split(" ")[2];
 		try {
-			if ( forbidden.indexOf(what) == -1 && bot.features[what] ) {
+			if ( bot.features[what] ) {
 				delete bot.features[what];
-				bot.say(raw.args[0], "Flushed storage for " + what);
+				out("Flushed storage for " + what);
 			} else {
 				var featureList = Object.keys(bot.features);
-
-				if(forbidden.indexOf(what) !== -1) throw Error("You're trying to flush an internal feature's storage. Please don't.");
 				if(!bot.features[what]) throw Error("Doesn't exist. Existing features: " + featureList);
 			}
 		} catch (e) {
-			bot.say(raw.args[0], "No storage exist for: "+what+". Error message: " + e.message);
+			out("No storage exist for: "+what+". Error message: " + e.message);
 		}
 	}
-	
-	//le krill
-	if( raw.command == "PRIVMSG" && raw.args[1].trim().substr(0,6) == "$ kill" && admins.indexOf(raw.user) !== -1 ) {
-		if( raw.args[1].trim().split(" ")[2] !== bot.nick ) { bot.say(raw.args[0], "Not me."); return; }
-		bot.disconnect("Kill requested by " + raw.user, function() {
-			process.exit("bye");
-		})
-	}
 
-	//le auth
-	if( raw.command == "PRIVMSG" && raw.args[1].trim() == "$ auth" && admins.indexOf(raw.user) !== -1 ) { 
-		bot.say("NickServ","GHOST "+ bot.features.config.config.register.username +" "+ bot.features.config.config.register.password);
-		bot.send("NICK", bot.features.config.config.register.username);
-		bot.say("NickServ","IDENTIFY " + bot.features.config.config.register.password );
-	}
-
-	//le part
-	if( raw.command == "PRIVMSG" && raw.args[1].trim() == "$ part "+ bot.nick && admins.indexOf(raw.user) !== -1 ) {
-		bot.part(raw.args[0],"Parting requested by " + raw.user);
-	}
-
-	//le join
-	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,8) == "$ invite") {
-		bot.join(raw.args[1].split(" ")[2] , function() {
-			bot.say(raw.args[1].split(" ")[2], "Hello. I have been summoned by " + raw.user +". "+bot.nick+ " is ready!");
-		});
-	}
-
-	//le say
-	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,5) == "$ say" && admins.indexOf(raw.user) !== -1 ) {
-		bot.say(raw.args[1].split(" ")[2], raw.args[1].split(" ").slice(3).join(" "));
-	}
-
-	//le pipe (experimental and dangerous) 
-	if ( !(bot.features.pipes instanceof Array)  ) bot.features.pipes = []; 
-
-	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,6) == "$ pipe" && admins.indexOf(raw.user) !== -1 ) {
-		if( raw.args[1].split(" ").length < 4 ) { bot.say(raw.args[0], "Insufficient params."); return; }
-		var source = raw.args[1].split(" ")[2].trim();
-		var target = raw.args[1].split(" ")[3].trim();
-
-		if(source == bot.nick || target == bot.nick) { bot.say(raw.args[0], "Please don't."); return; }
-		bot.features.pipes.push( {src: source, targ: target} );
-		bot.say(raw.args[0], "Pipe initiated from: " +  source + " --> " + target);
-
-		//--- New Channel listener or nah?
-		if( bot.features.pipes.length === 1 && bot.features.pipesEnabled !== true ) { bot.features.pipesEnabled = true;
-			bot.addListener("raw", function(rawmsg) {
-				if( rawmsg.command !== "PRIVMSG" ) return;
-				bot.features.pipes.forEach( function(pipe) { 
-					if( rawmsg.args[1].substr(0,1) == "$" || rawmsg.command !== "PRIVMSG" ) return;
-					if( pipe.src == rawmsg.args[0] ) bot.say( pipe.targ,  rawmsg.args[1]);
-					if( pipe.src == rawmsg.nick && rawmsg.args[0] == bot.nick ) bot.say( pipe.targ,  rawmsg.args[1]);
-				});
-			});
-		} 
-	}
-
-	//le unpipe 
-	if( raw.command == "PRIVMSG" && raw.args[1].substr(0,8) == "$ unpipe" && admins.indexOf(raw.user) !== -1 ) {
-		if( raw.args[1].split(" ").length < 4 ) { bot.say(raw.args[0], "Insufficient params."); return; }
-		var source = raw.args[1].split(" ")[2].trim();
-		var target = raw.args[1].split(" ")[3].trim();
-		var removed = 0;
-		bot.features.pipes.forEach( function(pipe, index) {
-			if( pipe.src == source && pipe.targ == target ) {
-				bot.features.pipes.splice(index,1); removed++;
-			}
-		});
-		var plural = removed > 1? "s": "";
-		if( removed > 0 ) bot.say(raw.args[0], "Found "+removed+" occurence"+plural+". Pipe has been dismantled.");
-		if( removed == 0 ) bot.say(raw.args[0], "Did not found any pipe with such configuration.");
-	}	
 }
